af3f621169c8d81c3901b23cb1a09c5642dd5cc4
dasWarder <Al.94_94@mail.ru>
2021-09-21 10:17:15 +0300

    Add token and refresh token implementation

--- src/main/java/com/example/spring_security_jwt_auth/service/UserServiceImpl.java
+++ src/main/java/com/example/spring_security_jwt_auth/service/UserServiceImpl.java
@@ -6,25 +6,37 @@ import com.example.spring_security_jwt_auth.repository.RoleRepository;
 import com.example.spring_security_jwt_auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
   private final UserRepository userRepository;
 
   private final RoleRepository roleRepository;
 
+  private final PasswordEncoder passwordEncoder;
+
   @Override
   public User saveUser(User user) {
 
     log.info("Store a user");
+    String encodedPass = passwordEncoder.encode(user.getPassword());
+    user.setPassword(encodedPass);
 
     return userRepository.save(user);
   }
@@ -60,5 +72,31 @@ public class UserServiceImpl implements UserService {
     log.info("Receive users");
 
     return userRepository.findAll();
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+    User user = userRepository.findUserByUsername(username);
+
+    if (user == null) {
+
+      log.info("The user with a username = {} not found", username);
+      throw new UsernameNotFoundException("The username not found");
+
+    } else {
+
+      log.info("Load a user with the username = {}", username);
+    }
+
+    Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+    user.getRoles()
+        .forEach(
+            role -> {
+              authorities.add(new SimpleGrantedAuthority(role.getName()));
+            });
+
+    return new org.springframework.security.core.userdetails.User(
+        user.getUsername(), user.getPassword(), authorities);
   }
 }
